66f23649d090a04edaa4ddd136e795b2c47c15ac
Takada-Tomomasa <67568084+Takada-Tomomasa@users.noreply.github.com>
2020-11-09 18:14:36 +0900

    Add files via upload

---
+++ components/Generic.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="generic-container">
+    <h1>何作ろう？</h1>
+    <li>自己紹介ページ</li>
+    <li>どこかのホームページをパクる</li>
+    <li>TODO List</li>
+    <li>なんかのマイペ</li>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Generic'
+}
+
+</script>
+
+<style>
+.generic-container {
+  text-align: left;
+  background-color:tomato;
+  margin-left: 400px;
+  margin-right: 400px;
+}
+</style>
---
+++ pages/ApplicationMainPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <div class="title">
+      アプリのメインページ
+    </div>
+    <p>ここに色々実験していこう！</p>
+    <input v-model="commentNumber">
+    <p>{{ commentData[commentNumber] }}</p>
+    <Todos :commentData="cdata" />
+    <!-- <Generic /> -->
+  </div>
+</template>
+
+<script>
+import Todos from '../components/Todos'
+
+export default {
+  name: 'ApplicationMainPage',
+  components: {
+    Todos
+  },
+  data () {
+    return {
+      commentData: [],
+      commentNumber: null
+    }
+  },
+
+  mounted () {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+      .then(response => response.json())
+      .then(json => (this.commentData = json))
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+}
+
+</style>
